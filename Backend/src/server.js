8f3bc78e8f4dba41b5751e70a234136476ab4637
Manav Khare <manavkhare121@gmail.com>
2026-01-31 04:09:37 +0530

    initialize backend with Express, Socket.IO, and Google GenAI integration

---
+++ Backend/src/server.js
@@ -0,0 +1,29 @@
+import app from "./app.js"
+import dotenv from "dotenv"
+import { createServer } from "http";
+import { Server } from "socket.io";
+import generateresponse from "./services/ai.services.js"
+import { response } from "express";
+dotenv.config({
+    path: './.env'
+});
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {    //Io means whole server but socket means only one user
+  console.log("A user connected")
+  socket.on("disconnected",()=>{
+    console.log("A User disconnected")
+  })
+
+  socket.on("ai-message", async (data)=>{
+    const response=await generateresponse(data.prompt)
+    console.log("AI Response",response)
+    socket.emit("ai-message-response",{response})
+  })
+});
+
+httpServer.listen(process.env.PORT || 3000,()=>{
+    console.log(`server is running at port:${process.env.PORT}`)
+}) 
